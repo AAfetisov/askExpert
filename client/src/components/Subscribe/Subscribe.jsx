c2256679e7a7ec90060a10d7f348b8e79070bf12
vladmalovich <waldemar.malovichko@gmail.com>
2023-03-17 09:22:44 +0300

    added Subscribes tab alomost done

--- client/src/components/Subscribe/Subscribe.jsx
+++ client/src/components/Subscribe/Subscribe.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { Table } from 'react-bootstrap';
-import style from './Subscribe.module.css';
+import styles from './Subscribe.module.css';
 
 export default function Subscribe() {
   const [offer, setOffer] = useState([]);
@@ -25,22 +25,44 @@ export default function Subscribe() {
   return (
     <>
       <br />
-      <div>My subscribe</div>
+      <br />
+      <br />
+      <div className={styles.subscribedOffer}>My subscribe</div>
+      <div className={styles.subscribedOffer}>
+        {offer?.length
+            && offer?.map((of) => <div key={of.id}>{of.Question.title}</div>)}
+      </div>
+      <div className={styles.subscribedOffer}>
+        {offer?.length
+            && offer?.map((of) => <div key={of.id}>{of.Question.User.email}</div>)}
+      </div>
+      <div>
+        {offer?.length
+            && offer?.map((of) => <div key={of.id}>{of.text}</div>)}
+      </div>
+      <div>
+        {offer?.length
+            && offer?.map((of) => <div key={of.id}>{of.price}</div>)}
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <Table striped="columns">
         <thead>
           <tr>
-            <th>Tags    </th>
-            <th>Offer    </th>
-            <th>Expert    </th>
-            <th>Title    </th>
-            <th>Text    </th>
-            <th>Price    </th>
+            <th>Title</th>
+            <th>Username</th>
+            <th>Tags</th>
+            <th>Text</th>
+            <th>Price</th>
           </tr>
         </thead>
         <tbody>
           {(offer.length
             && offer.map(({
-              Subjects, Offers, id, expertId, questionId, title, text, price,
+              Subjects, Offers, id, Username, title, text, price,
             }) => (
               <tr key={id}>
                 <td>{Subjects?.map((el1) => el1.title)}</td>
@@ -48,8 +70,6 @@ export default function Subscribe() {
                 <td>
                   <Link to={`/subscribe/${id}`}>{title}</Link>
                 </td>
-                <td>{expertId}</td>
-                <td>{questionId}</td>
                 <td>{text}</td>
                 <td>{price}</td>
               </tr>
