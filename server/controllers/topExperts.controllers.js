4cced0290907aebcaa91d2b78dd2d03c2e43abd3
Olga <olvik1605@gmail.com>
2023-03-22 12:38:55 +0300

    rate release 3

--- server/controllers/topExperts.controllers.js
+++ server/controllers/topExperts.controllers.js
@@ -3,19 +3,25 @@ const question = require('../db/models/question');
 
 const ShowTopExperts = async (req, res) => {
   try {
+    // const expertId = await Rating.findAll({
+    //   attributes: ['expertId'],
+    //   raw: true,
+    //   nested: true,
+    // });
+    // console.log(expertId, '________>exId');
     const allExperts = await Rating.findAll({
       attributes: ['expertId',
         [sequelize.fn('AVG', sequelize.col('rating')), 'averageRating'],
       ],
       include: [
-        { model: User, attributes: ['id', 'name', 'surname', 'userpic'] }],
+        { model: User, attributes: ['id', 'name', 'surname', 'userpic'], where: { id: expertId } }],
       group: ['expertId', 'User.id', 'User.name', 'User.surname', 'User.userpic'],
       raw: true,
       nested: true,
       order: [['averageRating', 'DESC']],
     });
 
-    // console.log(allExperts, '======>AllExperts');
+    console.log(allExperts, '======>AllExperts');
 
     res.json(allExperts);
   } catch (error) {
