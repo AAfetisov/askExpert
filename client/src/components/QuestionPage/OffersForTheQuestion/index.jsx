90c41511c4b005dea0d9d104922a5fe6a1283c1d
Александр Фетисов <roumor@gmail.com>
2023-03-24 18:16:22 +0700

    final version

--- client/src/components/QuestionPage/OffersForTheQuestion/index.jsx
+++ client/src/components/QuestionPage/OffersForTheQuestion/index.jsx
@@ -4,7 +4,7 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/prop-types */
 import { Avatar } from '@mui/material';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Modalwindow from './Modal';
 import style from './style.module.css';
 
@@ -13,6 +13,7 @@ export default function OffersForTheQuestion({ questionId, setRecipientId }) {
   const [elementClicked, setElementClicked] = useState(null);
   const [stylebtn, setStylebtn] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [currentOfferId, setCurrentOfferId] = useState(0);
   useEffect(() => {
     const abortController = new AbortController();
     const { signal } = abortController;
@@ -55,57 +56,62 @@ export default function OffersForTheQuestion({ questionId, setRecipientId }) {
       }),
     });
     if (transaction.ok) {
-      const result = transaction.json();
       setStylebtn(true);
     } else {
       console.log('failed transaction');
     }
   };
-  // const handleClickModal = () => {
-  //   setShowModal(true);
-  // };
-  // const handleConfirm = () => {
-  //   handelPay();
-  //   setShowModal(false);
-  // };
-  // const handleCancel = () => {
-  //   setShowModal(false);
-  // };
+
+  const handleClickModal = (offerId) => {
+    setCurrentOfferId(offerId);
+    setShowModal(true);
+  };
+  const handleConfirm = () => {
+    handelPay(currentOfferId);
+    setShowModal(false);
+  };
+  const handleCancel = () => {
+    setCurrentOfferId(0);
+    setShowModal(false);
+  };
 
   if (offers.length <= 0) { return (<div className={style.blockTitle} style={{ backgroundColor: 'grey' }}>No offers yet</div>); }
   return (
-    <div className={style.offersContainer}>
+    <>
+      <Modalwindow
+        show={showModal}
+        message="Are you sure you want to continue?"
+        onConfirm={handleConfirm}
+        onCancel={handleCancel}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      />
+      <div className={style.offersContainer}>
 
-      <div className={style.blockTitle}>Help offered:</div>
-      <ul className={style.offers}>
-        {offers?.length && offers.map((of) => (
-          <a key={of.id} href="dummy" onClick={(e) => handleOfferClick(of.id, of.expertId, e)}>
-            <li className={style.offer}>
-              {/* <img alt="upic" className={style.userpic} src={`${of.User.userpic}`} /> */}
-              <Avatar
-                alt="Remy Sharp"
-                src={of.User.userpic || 'https://cdn-icons-png.flaticon.com/512/3177/3177440.png'}
-                sx={{ width: 30, height: 30 }}
-              />
-              <span className={style.userEmail}>{of.User.name}</span>
-              <span className={style.userEmail}>{of.User.surname}</span>
-              <span className={style.price}>{of.price}</span>
+        <div className={style.blockTitle}>Help offered:</div>
+        <ul className={style.offers}>
+          {offers?.length && offers.map((of) => (
+            <a key={of.id} href="dummy" onClick={(e) => handleOfferClick(of.id, of.expertId, e)}>
+              <li className={style.offer}>
+                {/* <img alt="upic" className={style.userpic} src={`${of.User.userpic}`} /> */}
+                <Avatar
+                  alt="Remy Sharp"
+                  src={of.User.userpic || 'https://cdn-icons-png.flaticon.com/512/3177/3177440.png'}
+                  sx={{ width: 30, height: 30 }}
+                />
+                <span className={style.userEmail}>{of.User.name}</span>
+                <span className={style.userEmail}>{of.User.surname}</span>
+                <span className={style.price}>{of.price}</span>
 
-              <button type="button" disabled={stylebtn} onClick={() => handelPay(of.id)} className={stylebtn ? 'solvedBtn' : 'solvedBtn greyBtn'}>Pay Expert</button>
+                {/* <button type="button" disabled={stylebtn} onClick={() => handelPay(of.id)} className={stylebtn ? 'solvedBtn' : 'solvedBtn greyBtn'}>Pay Expert</button> */}
 
-              {/* <button type="button" disabled={stylebtn} onClick={handleClickModal} className={stylebtn ? 'solvedBtn' : 'solvedBtn greyBtn'}>Pay Expert</button>
-              <Modalwindow
-                show={showModal}
-                message="Are you sure you want to continue?"
-                onConfirm={handleConfirm}
-                onCancel={handleCancel}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-              /> */}
-            </li>
-          </a>
-        ))}
-      </ul>
-    </div>
+                <button type="button" onClick={() => handleClickModal(of.id)} className={stylebtn ? 'solvedBtn' : 'solvedBtn greyBtn'}>Pay Expert</button>
+
+              </li>
+            </a>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
